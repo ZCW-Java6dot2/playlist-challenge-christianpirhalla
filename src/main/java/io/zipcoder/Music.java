d36416650548b5e104f23f5744fd91f18ffc4d15
Christian Pirhalla <chrispir@udel.edu>
2020-11-26 10:02:21 -0500

    lab complete

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -9,6 +9,26 @@ public class Music {
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+        //count forward
+        int stepsForward = 0;
+        int i = startIndex;
+        while (true){ // we can do this bc the song is guaranteed to be in the playlist
+            if (playList[i].equals(selection)) {break;}
+            stepsForward++;
+            if (i == playList.length - 1){ i = 0; } // loop back to the beginning if we're at the end
+            else { i++; }
+        }
+
+        //count backward
+        int stepsBackward = 0;
+        i = startIndex;
+        while (true){
+            if (playList[i].equals(selection)) {break;}
+            stepsBackward++;
+            if (i == 0){ i = playList.length - 1; } // loop back to the end if we're at the beginning
+            else { i--; }
+        }
+
+        return Math.min(stepsBackward, stepsForward);
     }
 }
